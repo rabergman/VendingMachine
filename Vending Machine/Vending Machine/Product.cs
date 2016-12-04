72a9fd8febe7be0379bdfa01f4614c4c17cc1d03
Ronda Bergman <rondabergman@hotmail.com>
2016-12-03 23:17:32 -0500

    Created products

---
+++ Vending Machine/Vending Machine/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine
+{
+    public abstract class Product
+    {
+        /// <summary>
+        /// The name of the product
+        /// </summary>
+        public string Name { get; protected set; }
+
+        public decimal Price { get; protected set; }
+    }
+
+    public class Cola : Product
+    {
+        public Cola()
+        {
+            this.Name = "Cola";
+            this.Price = 1.00M;
+        }
+    }
+
+    public class Chips : Product
+    {
+        public Chips()
+        {
+            this.Name = "Chips";
+            this.Price = .50M;
+        }
+    }
+
+    public class Candy : Product
+    {
+        public Candy()
+        {
+            this.Name = "Candy";
+            this.Price = .65M;
+        }
+    }
+}
