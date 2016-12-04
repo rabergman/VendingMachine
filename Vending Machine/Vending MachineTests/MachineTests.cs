74d4ac12eed44f046d8b853fc858cde451e009c4
Ronda Bergman <rondabergman@hotmail.com>
2016-12-04 01:27:36 -0500

    Fixing tests

--- Vending Machine/Vending MachineTests/MachineTests.cs
+++ Vending Machine/Vending MachineTests/MachineTests.cs
@@ -11,6 +11,9 @@ namespace Vending_Machine.Tests
     [TestClass()]
     public class MachineTests
     {
+        const int numProducts = 2;
+        const int numCoins = 2;
+
         [TestMethod()]
         public void MachineTest()
         {
@@ -29,14 +32,51 @@ namespace Vending_Machine.Tests
 
             //TODO: Add code to empty products so refill can be tested
 
-            Assert.Fail();
+            int chipCount = 0;
+            int colaCount = 0;
+            int candyCount = 0;
+
+            //Determine the number of each product to be added
+            foreach (var item in machine.ProductInInventory)
+            {
+                if (item.GetType() == typeof(Chips))
+                    chipCount++;
+                else if (item.GetType() == typeof(Cola))
+                    colaCount++;
+                else if (item.GetType() == typeof(Candy))
+                    candyCount++;
+            }
+
+            Assert.AreEqual(numProducts, chipCount);
+            Assert.AreEqual(numProducts, colaCount);
+            Assert.AreEqual(numProducts, candyCount);
         }
 
         [TestMethod()]
         public void RefillCoinsTest()
         {
+            Machine machine = new Machine();
+
             //TODO: Add code to empty coins so refill can be tested
-            Assert.Fail();
+
+            int nickelCount = 0;
+            int dimeCount = 0;
+            int quarterCount = 0;
+
+            //Determine the number of each coin to be added
+            foreach (var item in machine.CoinsInInventory)
+            {
+                if (item.GetType() == typeof(Nickel))
+                    nickelCount++;
+                else if (item.GetType() == typeof(Dime))
+                    dimeCount++;
+                else if (item.GetType() == typeof(Quarter))
+                    quarterCount++;
+            }
+
+            Assert.AreEqual(numCoins, nickelCount);
+            Assert.AreEqual(numCoins, dimeCount);
+            Assert.AreEqual(numCoins, quarterCount);
         }
     }
 }
