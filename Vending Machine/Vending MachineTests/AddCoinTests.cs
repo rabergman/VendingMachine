cc64c610d0eddd020e3590a309854c5df611f0c1
Ronda Bergman <rondabergman@hotmail.com>
2016-12-03 23:12:58 -0500

    Added selection tests

---
+++ Vending Machine/Vending MachineTests/AddCoinTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vending_MachineTests
+{
+    [TestClass]
+    public class AddCoinTests
+    {
+        [TestMethod]
+        public void AddCoin()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Vending Machine/Vending MachineTests/SelectProductTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vending_MachineTests
+{
+    [TestClass]
+    public class SelectProductTests
+    {
+        [TestMethod]
+        public void SelectColaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SelectChipsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SelectCandyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
