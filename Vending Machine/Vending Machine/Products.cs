2751a2d0c227dddcaaa465eb52a4e7a0f04e2f59
Ronda Bergman <rondabergman@hotmail.com>
2016-12-04 20:44:55 -0500

    A little more cleanup

--- Vending Machine/Vending Machine/Products.cs
+++ Vending Machine/Vending Machine/Products.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vending_Machine
+﻿namespace Vending_Machine
 {
     /// <summary>
     /// Parent class of all products
